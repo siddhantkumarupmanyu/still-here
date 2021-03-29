60d5f78ce12db0fa1a7ae0896cb60afa43d240e8
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2021-03-29 22:48:35 +0530

    es6 works llike this

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
-
+import { Templates } from "./Templates.js";
 
 function buttonClick(type: string) {
     let mainContainer = document.getElementById("main") as HTMLElement;
 
     if (type === "play") {
-        mainContainer.innerHTML = `<button onclick="buttonClick('end')">End</button>`;
+        mainContainer.innerHTML = Templates.quizPage();
     }
     else if (type === "end") {
-        mainContainer.innerHTML = `<p>Score 0</p>`;
+        mainContainer.innerHTML = Templates.scorePage();
     }
 
 }
+
+const main = document.querySelector("#main") as HTMLElement;
+const button = main.children[0];
+button.addEventListener('click', function () {
+    buttonClick("play");
+});
