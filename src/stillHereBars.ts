838b49c7311f81245cc29024c4cc921df71ed04b
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2021-04-02 20:49:17 +0530

    done stillHereBars.ts

--- src/stillHereBars.ts
+++ src/stillHereBars.ts
@@ -238,19 +238,462 @@ export const stillHereBarsObj = {
         Dilli Ke Launde Hum Hain Kutti Firim Bhai`
 
     ],
-    
-    "key3": [
-        "key3-value0",
-        "key3-value1",
-        "key3-value2",
-        "key3-value3",
-        "key3-value4"
+
+    "Dream": [
+        `14 saal, London sheher
+        Likha pehla rap
+        Wo tha moment
+        Fell in love the pen and pad`,
+
+        `Mere paas bhi tha will jaise Men in black
+        School me dete dhamki me bhi sunki
+        I just send it back`,
+
+        `School ka sabse popular rapper tha Troy
+        Radio pe uske gaane everybody knew the boy`,
+
+        `Usne naam diya mujhe, undercover thug
+        Kyunki shakal se sharif but on the mic I fuck it up`,
+
+        `Validation thodi aane lagi haath
+        School me gangster dete bhav, karne lage mujhse baat`,
+
+        `Ibrahim bana dost, sunta Cormega aur Nas
+        Uski wajah se mene jaake recording kari start`,
+
+        `Barnfield estate me tha kisi ka stu
+        Jisme gaana record karne ki icha thi meri khoob`,
+
+        `Toh Naam rakha Phyxer aur banaya mene crew
+        Bane hum ‘The Royal Rappers’ in 2002`,
+
+        `I Just had a dream
+        Since I was just a teen, karna chahta tha me rap
+        Bana na chahta tha me scene`,
+
+        `Yea I Just had a dream
+        Jub inko na yakeen, put my city on my back
+        Aaj scene hai haseen`,
+
+        `Charcha hone lagi, badne lagi baat
+        Mujhe apnane lage - ladke jo school me the hard`,
+
+        `(That’s Krsna, he’s safe man, he can rap init -
+        Oi Krsna bruv, can you rap for a minute)`,
+
+        `Aise baatein sunke me bhi hone laga khush
+        Hanging with the crooks, meri badali puri look`,
+
+        `Baggy tracks, air max on my foot, rock karta
+        Parka Jacket with the fur on the hood`,
+
+        `Ab MC Phyxer hogaya addict sa
+        Kharida sasta mic aur lagaya on the mixer`,
+
+        `Record kare jaakee mene tape pe tape
+        Verses the ek se ek, dekh I know you get the picture`,
+
+        `Me tha khush, mene dhoondi apni calling
+        Raat ko pirate radio stations pe karta call in`,
+
+        `Ab Jaise hi school me mujhe thori pehchaan mili
+        Next thing I know, I’m on a flight back to delhi (what the fuck)`,
+
+        `Umar 16, dilli bhi lagne laga ab naya
+        Sochta tha me aksar yaar kidhar fas gaya`,
+
+        `Jo banaya wo gawaya, mujhe bahut fikar
+        Kyunki Kiski ko hip hop ka na thoda bhi shauk idhar`,
+
+        `School ki copy ke peeche likhta tha lyrics
+        Apne favourite rappers ke flow bhi Karta tha me mimic`,
+
+        `Aise nikale 2 saal, I thought it was finished
+        Par 18 pe college me meri Banane lagi image`,
+
+        `Mile Su.1 aur 2D, jinhe rap ka tha shauk
+        Hum ne banaya ek group - Illicit Cash Mobb`,
+
+        `2005, Record kiye Kuch gaane humne
+        Hip Hop Nights Delhi me, log bhi pehchane hume`,
+
+        `Young and hungry, wo din ab lagte hai best
+        We Didn’t rap for the cheques, we just rapped for respect`,
+
+        `Fir 2006 me, solo artist bana me next
+        Naam rakha Prozpekt, I guess you know the rest`
+
     ],
-    "key4": [
-        "key4-value0",
-        "key4-value1",
-        "key4-value2",
-        "key4-value3",
-        "key4-value4"
+
+    "Fall Off": [
+        `Yo
+        They think about the fall off`,
+
+        `Bolre hai KR$NA ki hype khatam hori hai bro
+        KR$NA ki hype toh bas beef se aayi thi`,
+
+        `Hype bane jabse meri hype bane
+        Life me na mile mujhe, mere sage bhai bane`,
+
+        `Mind kare par kabhi na grind kare
+        Genocide lage jab mic pe meri thodi vibe bane`,
+
+        `Killer bars aaj, mujhe Bhide paanch saat
+        Unke mille yahan laash, ye na mereliye khaas baat`,
+
+        `Mile yahan naag, dil he aaj saaf
+        Har kill hai vardaat, mera skill hai shandaar`,
+
+        `Gand fadh, karoon me tandav chal, charoo me chand ab
+        Karoon me kaand ab kal`,
+
+        `Chahiye beef, banoo me sand ab, toddu maidan
+        Ab bane kyun janwar sab? lage kyun Bandhav Garh?`,
+
+        `Har beat pe killer bana marzi se
+        Cold mera flow toh shabd bhi mere barfeele`,
+
+        `Chahiye inhe smoke, toh lo banoonga charsee me
+        Baatein karke jo provoke kare wo barfi hai`,
+
+        `Inke barre mujhe fikar na hai khaas
+        I might kill ‘em with the venom, when I hit ‘em in the heart`,
+
+        `I'm Developing a rhythm, n I get em with the bars
+        Ye to sentimental rappers Inka Jigar nai hai hard`,
+
+        `Ooof, I’m on to the next
+        Gentleman I'm killing these rappers that want to test`,
+
+        `Murder instrumentals, fir karoo na zyaada flex
+        Inhe kabar me sula doo, see I put ‘em all to rest`,
+
+        `Ye to bhai
+        Karta rahoonga me day or night`,
+
+        `Move, Thoda dey do side
+        Bhage mere aage
+        Jaise ye off-side(Oooh)`,
+
+        `Hum me fark hi hai yun
+        Dekh ke mujhe teri jalti hai, kyun?`,
+
+        `Baat chali - farzi hai tu
+        Dil mera cold, pooche sardi hai kyun?`,
+
+        `Aaj kal Saare bane mere critic
+        Kare badi baatein but they can never live it`,
+
+        `Agar li hai mene side, then I ride to defend it
+        Label dekhe mene bahaut but the grind is independent`,
+
+        `Came back
+        Jab inhe lagne laga that i'm finished
+        Itne saal ki mehnat, kare judge in a minute`,
+
+        `Mere wajeh se behtar kare ye gande se lyrics
+        Inke dhande hai gimmick, ye to bande hai timid`,
+
+        `Back to back ye fact, meri limit nahi
+        Karta hoo track pe attack with ability`,
+
+        `Fact toh ye whack hai, me rap ka epitome
+        Act kare mad, But they actually feeling me`,
+
+        `Mujhe Aksar, bolte the buss kar
+        Rah me dat kar, gaya nahi daftar`,
+
+        `Har avsar pe lagta me officer
+        Ye pen militant, har akshar hai lashkar`,
+
+        `They thought I’m bout to fall off
+        Now they got plans they gotta call off`,
+
+        `Came in the game like a 12 gadge pump
+        That's a gun that's sawed off`,
+
+        `They thought I'm bout to fall off
+        Now they got plans they gotta call off`,
+
+        `Said my flow kinda hot, but I ain't tryna talk
+        If she don't take it all off`
+
+    ],
+
+    "Saza-e-Maut": [
+
+        `Dekh ke mujhe mat bhaunk
+        Ek shabd bole opp, that’s too much talk`,
+
+        `G town boys, mera block
+        Kare rep Southwest, that’s too much Sauce`,
+
+        `Dil me bhara hai khauf, tu bada hai soft
+        Tu dara hai bahut`,
+
+        `Inko chada hai shauk, toh laga hai chalk
+        Ye gaana hai Saza-e maut`,
+
+        `Mere sheher me launde hazir
+        Kaafi kaabil, gang me dakhil`,
+
+        `Rappers karte baatein
+        I don’t care what who’s saying like Nasir`,
+
+        `Savage - jahil
+        Kaam me maahir, beat pe lagta shayar`,
+
+        `Tu dil se kayar, mil ke ghayal
+        Drive by - swift desire`,
+
+        `Kya chall raha hai bol? (bol)
+        Yahan halla hai shor`,
+
+        `Mere sheher me log kare spray
+        You can catch these Strays like Frendicoes`,
+
+        `Tell me, who run it?
+        Bandook chale Bam jaise Bhuvan hai`,
+
+        `Dilli sheher, if you really want smoke
+        Yahan karega choke - Pardushan hai`,
+
+        `Baatein inki lagi mujhe bekaar
+        Agar naam nahi liya mera ek baar`,
+
+        `If it’s dollar sign then it’s K.R
+        Rehman, deta shot jaise A.R`,
+
+        `Balling - Neymar
+        Drilling - ek chaar`,
+
+        `Me na victim, I’m more like Vikram, might pull up with
+        A ghost, that’s Betaal`,
+
+        `Kaam aitihaasik (atihaasik)
+        Mukaam mene kiya hai haasil`,
+
+        `Kaise aakhir?
+        Mic pe bana me kaatil (bana me kaatil)`,
+
+        `Tu bana kyun naag, tu bana kyun saanp
+        Tu bane kyun shaatir? (shaatir)`,
+
+        `Tu aa raha kyun paas, tu kar raha kyun baat?
+        Tu sada kyun jaake? (haan)`,
+
+        `Gala ye ghot, gala jo chala to saza-e-maut
+        Gala ye goat, gala jo chala to saza-e-maut`,
+
+        `Ab hongi milke baate
+        Dilli me milenge yeh til milate`,
+
+        `Gun wun wali karein filmi baate
+        Mile dhan dhan bhardunga inme chaate`,
+
+        `Bin bata ke milunga na din batake
+        Gin lagake milunga na gym lagake`,
+
+        `Jimme baat hai mere mile jisse jagah pe
+        Jhanjhanaat hai kano me bin patakhe`,
+
+        `Yeh mera farz hai
+        Inka to purge hi marz hai`,
+
+        `Mujhpe na khane ko tars hai
+        Inko chatane ko farsh hai`,
+
+        `Ab na yeh chuhon ka varsh hai
+        (OX OX)`,
+
+        `Humne banaya game tumne lagaya game
+        We are not the same`,
+
+        `Ab lu me kiska name
+        Too many snakes to blame`,
+
+        `Lagu me pop
+        Karu me smoke
+        Bodies we drop`,
+
+        `Thane aur court
+        Khane me note
+        Get at your throat`,
+
+        `Loco par karte ni coke
+        Ina-the bando we dope`,
+
+        `Na na na we never broke
+        Winners neva eva neva eva eva broke`
+    ],
+
+    "Na Hai Time": [
+
+        `Mere paas
+        Na hai time
+        Call kare par, busy meri line`,
+
+        `Subah shaam
+        On the grind
+        Jab tak dikhe nahi mujhe dollar sign (dollar sign)`,
+
+        `Ooh ooh bolne lage jhoot
+        Ooh ooh dekha inka asli roop`,
+
+        `Ooh ooh lage kaafi naram ye
+        Bolte saare KR$NA to booth me kaafi garam hai`,
+
+        `Call karte mujhe jaise hum hai dost (ya)
+        Tum na jante mujhe, hum na kabhi close yaar`,
+
+        `Ye Zindagi hai meri, ye na koi joke
+        Issiliye muskuraata nahi karta jub me pose, (ya)`,
+
+        `Flash, kare mujhe flash, merе paas na hai time
+        If it ain’t about the cash`,
+
+        `Boli boy, Kar relax
+        You need to havе some fun
+        Wo karna chahti aish par`,
+
+        `Tu na jaane mujhe khaas bro
+        Fly me lagoo jaise jhaaz ho`,
+
+        `Log bolte sirf paisa karta baat to
+        Fir kyun yahan gawah raha apni saans ko?`,
+
+        `Oh no oh no
+        Tu hai kaun, I don’t know`,
+
+        `Chahiye money, chahiye fame
+        Chahiye dono`,
+
+        `Bolo bolo, Kahan hai jo jo
+        Daga deke gaye
+        Ab na dikhte wo log`,
+
+        `Duniya mene peeche chodi
+        Bole speed kar dheere thodi`,
+
+        `Haqeeqat boli
+        Lagta meri jeet ab hogi`,
+
+        `Me aur tu na homies, hum na brodies
+        You don’t know me`,
+
+        `Bhagoo teri chori. karke chori
+        Nirav Modi`
+
+    ],
+
+    "Living Legend": [
+
+        `Kismat ne roka
+        Par lad ke hum aaye
+        Jo bole namumkin,
+        Hum kar ke dikhaye`,
+
+        `Karenge ye yaad
+        Agar yahan se hum jaaye
+        Iss khel main hum legend,
+        And legends never die`,
+
+        `Lelo salah meri
+        Ab kalakari mujhe fal hai de rahi`,
+
+        `Kalam chala toh teri kala ko jala degi
+        Galan teri na suchi hai tu toh bada veli
+        Inki jagah le li toh zindigi bhi ab daga deri (wow)`,
+
+        `haram kiya inka mene jeena
+        Bahaya khoon aur paseena
+        Sab kuch clear hai, aquafina
+        Like wow`,
+
+        `Net pe beth ke bole
+        One love, I don’t give a f**k
+        Serving these rappers up
+        Jaise mein serena, (wow)`,
+
+        `kon achiever,
+        Ye toh saaf hai kafi`,
+
+        `Aana taani kar ya taaka jhaaki
+        Ye kalam hai zyada bhari`,
+
+        `Tokiyo na mujhe
+        Kyo-tu marna chahta jaani
+        Game hai mujhe pyaari
+        I’m her hero shr my nagasaki`,
+
+        `Arey arey kyun lage ye dare dare
+        Dekhe mujhe ageh bade to sade pade`,
+
+        `Ye khade khade
+        Jale pade, hum aise nahi pale bade`,
+
+        `Mujhse kyun ye ladr mare?
+        Haar bhi ab inke gale chade`,
+
+        `Flow icey, mahaul lage moscow
+        Tera bhai lage baller, no cosco`,
+
+        `Dekhe yahan aate jaate maine boht log
+        That red dot will make you do a dash
+        Jaise mosre code`,
+
+        `Lagai class, likhai meri khaas
+        Kamyaabi ki hai bhook
+        Aur viraasat ki hai pyaas, haan`,
+
+        `Kali das, har gaana itihaas
+        Jaunga main aaj toh
+        Karenge mujhe yaad, haan`,
+
+        `Kathin raaste, tu jaanch le,
+        Hum hanfte nahi
+        Saanp bhare ghaas main
+        Hum faasle se bhagte nahi`,
+
+        `Chipe inkr raaz paise baant le
+        Ye naachte bhi`,
+
+        `Rap meri art toh
+        Phir ladunga samaj se hi`,
+
+        `Legends never die,
+        They rise, karenge fight
+        Ye lies kare divide par
+        Guys kare deny ye`,
+
+        `Grind kare bhai toh
+        Rise karenge high
+        Sachai karega write toh
+        Hype banega mic pe`,
+
+        `Deen imaam nahi khoya,
+        Maine paya jo boya`,
+
+        `Jitna chahya nahi khaaya
+        Phir bhi maine paya hai dhoka`,
+
+        `This city I ran,
+        Jaise hoon main ayatollah`,
+
+        `Tu high hai thoda
+        Aaya to laaya fire,
+        Ab safaya hoga`,
+
+        `Godh bhare memne,
+        Ab goat bane game main`,
+
+        `Sound kare same,
+        Ye toh lame lage name se`,
+
+        `Bhagwan mera namesake,
+        Jwala mera aim dekh`,
+
+        `Hall of fame nahi daalo
+        Mujhe hall of flame mein`
     ]
 }
